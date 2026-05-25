0d2ee852e09161ad4537274e057ba2b25fa8a90f
Neekhil Kissun <Neekhilkissun@gmail.com>
2026-05-25 20:41:49 +1200

    changes made

--- config/database.php
+++ config/database.php
@@ -31,7 +31,7 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
+        'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
